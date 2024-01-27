06c36725e706e73f0a7a402906737f6aa54cbb74
srisairaaj10 <85604853+srisairaaj10@users.noreply.github.com>
2024-01-27 20:37:37 +0530

    Add files via upload

---
+++ mongo.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#mongo exec 
+kubectl exec -it mongo-0 -- mongo
+
+#initiate 
+rs.initiate();
+sleep(2000);
+rs.add("mongo-1.mongo:27017");
+sleep(2000);
+rs.add("mongo-2.mongo:27017");
+sleep(2000);
+cfg = rs.conf();
+cfg.members[0].host = "mongo-0.mongo:27017";
+rs.reconfig(cfg, {force: true});
+sleep(5000);
+
+
+#use language
+
+use langdb;
+
+db.languages.insert({"name" : "csharp", "codedetail" : { "usecase" : "system, web, server-side", "rank" : 5, "compiled" : false, "homepage" : "https://dotnet.microsoft.com/learn/csharp", "download" : "https://dotnet.microsoft.com/download/", "votes" : 0}});
+db.languages.insert({"name" : "python", "codedetail" : { "usecase" : "system, web, server-side", "rank" : 3, "script" : false, "homepage" : "https://www.python.org/", "download" : "https://www.python.org/downloads/", "votes" : 0}});
+db.languages.insert({"name" : "javascript", "codedetail" : { "usecase" : "web, client-side", "rank" : 7, "script" : false, "homepage" : "https://en.wikipedia.org/wiki/JavaScript", "download" : "n/a", "votes" : 0}});
+db.languages.insert({"name" : "go", "codedetail" : { "usecase" : "system, web, server-side", "rank" : 12, "compiled" : true, "homepage" : "https://golang.org", "download" : "https://golang.org/dl/", "votes" : 0}});
+db.languages.insert({"name" : "java", "codedetail" : { "usecase" : "system, web, server-side", "rank" : 1, "compiled" : true, "homepage" : "https://www.java.com/en/", "download" : "https://www.java.com/en/download/", "votes" : 0}});
+db.languages.insert({"name" : "nodejs", "codedetail" : { "usecase" : "system, web, server-side", "rank" : 20, "script" : false, "homepage" : "https://nodejs.org/en/", "download" : "https://nodejs.org/en/download/", "votes" : 0}});
+db.languages.find().pretty();
+
+exit
+
---
+++ set-env-api.sh
@@ -0,0 +1,13 @@
+
+echo api for elb
+{API_ELB_PUBLIC_FQDN=$(kubectl get svc api -ojsonpath="{.status.loadBalancer.ingress[0].hostname}")
+until nslookup $API_ELB_PUBLIC_FQDN >/dev/null 2>&1; do sleep 2 && echo waiting for DNS to propagate...; done
+curl $API_ELB_PUBLIC_FQDN/ok
+echo
+}
+
+echo elb dns
+{
+API_ELB_PUBLIC_FQDN=$(kubectl get svc api -ojsonpath="{.status.loadBalancer.ingress[0].hostname}")
+echo API_ELB_PUBLIC_FQDN=$API_ELB_PUBLIC_FQDN
+}
